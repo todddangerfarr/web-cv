55a0e14373f040e9d5b26f2acc6a60fc6e1d95fa
Todd Farr <todd.farr@gmail.com>
2016-08-01 15:09:29 +0900

    Feature: Started migration to angular.js for frontend display.

---
+++ app/app.js
@@ -0,0 +1,14 @@
+// angular module
+(function(){ // wrapping the function in a closure **good habit
+  var app = angular.module('webCv', []);
+
+  // Web CV controller to populate info variable
+  app.controller('InformationController', ['$http', function($http) {
+    var cv = this;
+    cv.info = [];
+    $http.get('../assets/data/information.json').success(function(data) {
+      cv.info = data;
+    });
+  }]);
+
+})();
